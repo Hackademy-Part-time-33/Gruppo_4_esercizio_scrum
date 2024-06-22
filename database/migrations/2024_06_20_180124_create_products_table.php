174aa5370f55e7bb2773c1e6d7b4bada320c2a96
juriboragine <juri25@outlook.it>
2024-06-22 18:12:24 +0200

    crud finito

--- database/migrations/2024_06_20_180124_create_products_table.php
+++ database/migrations/2024_06_20_180124_create_products_table.php
@@ -6,26 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
-{
-    Schema::create('products', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->text('description')->nullable();
-        $table->decimal('price', 8, 2);
-        $table->integer('quantity');
-        $table->timestamps();
-    });
-}
-
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
